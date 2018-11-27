1d9b20fab42c281c77967aae9386e7630a766493
Shehabeldeen <shehabeldeen555@gmail.com>
2018-11-27 23:38:52 +0200

    get lyrics of song and print it

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
+import re
 import os
 import pygame
+import urllib.request
 import eyed3
 from tkinter.filedialog import askdirectory
 from song import Song
@@ -16,12 +18,27 @@ for files in os.listdir(directory):
     if files.endswith(".mp3"):
         path = os.path.realpath(files)
         audio = eyed3.load(path)
+        artist = str(audio.tag.artist)
+        artist = artist.lower()
         print(audio.tag.artist)
         print(audio.tag.title)
+        song = str(audio.tag.title).replace(".mp3", "").lower()
+        song = re.sub('[^0-9a-zA-Z]+', '', song)
+        url = "http://www.azlyrics.com/lyrics/" + artist + "/" + song + ".html"
+        text = urllib.request.urlopen(url)
+        lyrics = text.read()
+        lyrics = str(lyrics)
+        where_start = lyrics.find('<!-- Usage of azlyrics.com content by any third-party')
+        start = where_start + 150
+        where_end = lyrics.find('<!-- MxM banner -->')
+        end = where_end - 32
+        lyrics = lyrics[start:end].replace("<br>\\n", "\n")
+        print(lyrics)
         print(audio.tag.album)
         print(audio.tag.track_num)
         print(audio.tag.release_date)
-        print(audio.tag.lyrics)
+        audio.tag.lyrics.set(u"shehab")
+        print(audio.tag.lyrics[0].text)
         print(audio.tag.genre.name)
         print(audio.info.time_secs)
         # sng = Song(audio.tag.track_num)
