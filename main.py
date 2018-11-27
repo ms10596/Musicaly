d718420d7c13b0cef004691065a105372f996d33
Shehabeldeen <shehabeldeen555@gmail.com>
2018-11-27 20:40:35 +0200

    read songs from directory and read artist name of songs

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import sqlite3
+import os
+from tkinter import *
+import pygame
+import eyed3
+from tkinter.filedialog import askdirectory
+
 
 qry = open("db/musicaly.sql").read()
-# print(qry)
 conn = sqlite3.connect('db/musicaly.db')
 conn.executescript(qry)
+
+directory = askdirectory()
+os.chdir(directory)
+
+for files in os.listdir(directory):
+    if files.endswith(".mp3"):
+        realdir = os.path.realpath(files)
+        audio = eyed3.load(realdir)
+        print(audio.tag.artist)
 
