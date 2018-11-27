a0f1dbe7da0778ab498249ce27496a5acf1a3972
Mohamed Sayed <ms10596@gmail.com>
2018-11-27 11:23:29 +0200

    feat: save and load bands

--- band.py
+++ band.py
@@ -1,3 +1,20 @@
+import sqlite3
+
+
 class Band:
-    def load_band(self):
-        pass
+    def __init__(self, id):
+        self.id = id
+
+    def load(self):
+        conn = sqlite3.connect('musicaly.db')
+        s = conn.execute("""SELECT * FROM Band where id ={} """.format(self.id))
+        print(s.fetchall())
+
+    def save(self, name="", band_artist_id=""):
+        conn = sqlite3.connect('musicaly.db')
+        conn.execute("""INSERT INTO Band VALUES(?, ?, ?)""", (self.id, name, band_artist_id))
+        conn.commit()
+if __name__ == '__main__':
+    x = Band(0)
+    x.save("mshro3")
+    x.load()
