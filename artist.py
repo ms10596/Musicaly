8391591c725e1f32bbc574492e12dcaa7f10c213
Mohamed Sayed <ms10596@gmail.com>
2018-11-30 16:49:29 +0200

    feat: added dua lipa to artists

--- artist.py
+++ artist.py
@@ -11,19 +11,26 @@ class Artist:
         conn = sqlite3.connect('db/musicaly.db')
         s = conn.execute("""SELECT * FROM Artist where id ={} """.format(self.id))
         result = s.fetchall()
-        self.name = result[1]
-        self.dob = result[2]
+        print(result)
+        if len(result) == 0:
+            return "not found"
+        self.name = result[0][1]
+        self.dob = result[0][2]
+        conn.close()
 
     def save(self, name="", dob="", band_id=""):
         conn = sqlite3.connect('db/musicaly.db')
-        conn.execute("""INSERT INTO Artist VALUES(?, ?, ?,?)""", (self.id, name, dob, band_id))
+        conn.execute("""INSERT INTO Artist VALUES(?, ?, ?)""", (name, dob, band_id))
         conn.commit()
+        conn.close()
+
+    def __str__(self):
+        return str(self.name) + " " + str(self.dob)
 
 
 if __name__ == '__main__':
-    x = Artist(1)
-    # x.save("michael", "1/5/6", 0)
-
-    x.load()
-
-    # x.save("michael", "1/5/5", 1)
+    a0 = Artist(0)
+    print(a0.load())
+    print(a0)
+    # a0.save("dua lipa", "22/8/1995")
+    # print(a0)
