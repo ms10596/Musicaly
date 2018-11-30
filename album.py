26986adb748f34e592ec34ccb937a5feb3572cb7
Mohamed Sayed <ms10596@gmail.com>
2018-11-30 17:00:02 +0200

    feat:add album dua lipa

--- album.py
+++ album.py
@@ -5,27 +5,31 @@ class Album:
     def __init__(self, id):
         self.id = id
         self.title = None
-        self.band_id = None
+        # self.band_id = None
         self.songs_no = None
-        self.album_song_id = None
+        # self.album_song_id = None
 
     def load(self):
         conn = sqlite3.connect('db/musicaly.db')
         s = conn.execute("""SELECT * FROM Album where id ={} """.format(self.id))
         result = s.fetchall()
-        self.title = result[1]
-        self.band_id = result[2]
-        self.songs_no = result[3]
-        self.album_song_id = result[4]
+        self.title = result[0][1]
+        # self.band_id = result[2]
+        self.songs_no = result[0][2]
+        # self.album_song_id = result[4]
         # print(s.fetchall())
 
-    def save(self, title="", band_id="", song_no="", album_song_id=""):
+    def save(self, title="", song_no=""):
         conn = sqlite3.connect('db/musicaly.db')
-        conn.execute("""INSERT INTO Album VALUES(?, ?, ?, ?, ?)""", (self.id, title, band_id, song_no, album_song_id))
+        conn.execute("""INSERT INTO Album VALUES(?, ?, ?)""", (self.id, title, song_no))
         conn.commit()
+
+    def __str__(self):
+        return str(self.title) + " " + str(self.songs_no)
 
 
 if __name__ == '__main__':
-    x = Album(0)
-    x.save("ibneleel")
-    x.load()
+    a0 = Album(0)
+    # a0.save("dua lipa", 17)
+    a0.load()
+    print(a0)
