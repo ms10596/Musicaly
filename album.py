c9970f7cb390de42f14b74b51be3eb183b89325d
Mohamed Sayed <ms10596@gmail.com>
2018-11-30 18:18:24 +0200

    understanding what shehab said

--- album.py
+++ album.py
@@ -5,19 +5,14 @@ class Album:
     def __init__(self, id):
         self.id = id
         self.title = None
-        # self.band_id = None
         self.songs_no = None
-        # self.album_song_id = None
 
     def load(self):
         conn = sqlite3.connect('db/musicaly.db')
         s = conn.execute("""SELECT * FROM Album where id ={} """.format(self.id))
         result = s.fetchall()
         self.title = result[0][1]
-        # self.band_id = result[2]
         self.songs_no = result[0][2]
-        # self.album_song_id = result[4]
-        # print(s.fetchall())
 
     def save(self, title="", song_no=""):
         conn = sqlite3.connect('db/musicaly.db')
--- band.py
+++ band.py
@@ -5,22 +5,24 @@ class Band:
     def __init__(self, id):
         self.id = id
         self.name = None
-        self.band_artist_id = None
 
     def load(self):
         conn = sqlite3.connect('db/musicaly.db')
         s = conn.execute("""SELECT * FROM Band where id ={} """.format(self.id))
         result = s.fetchall()
-        self.name = result[1]
-        self.band_artist_id[2]
+        self.name = result[0][1]
 
-    def save(self, name="", band_artist_id=""):
+    def save(self, name=""):
         conn = sqlite3.connect('db/musicaly.db')
-        conn.execute("""INSERT INTO Band VALUES(?, ?, ?)""", (self.id, name, band_artist_id))
+        conn.execute("""INSERT INTO Band VALUES(?, ?)""", (self.id, name))
         conn.commit()
+
+    def __str__(self):
+        return self.name
 
 
 if __name__ == '__main__':
     x = Band(0)
-    x.save("mshro3")
+    # x.save("mshro3")
     x.load()
+    print(x)
