2e4ccad55145c27257086fc20a4b138a1a5490a1
mohamed magdy <mohamed14magdi@gmail.com>
2018-12-04 21:24:49 +0200

    Created the grid

--- song.py
+++ song.py
@@ -34,7 +34,7 @@ class Song:
     def save(self, name="", release_date="", lyrics="", length=""):
         conn = sqlite3.connect('db/musicaly.db')
         params = (self.id, name, release_date, lyrics, length)
-        conn.execute("INSERT INTO Song (id,name, release_date, lyrics, length) VALUES (?,?, ?, ?, ?)", params)
+        conn.execute("INSERT INTO Song (id, name, release_date, lyrics, length) VALUES (?, ?, ?, ?, ?)", params)
         conn.commit()
 
     def __str__(self):
@@ -93,13 +93,13 @@ class Song:
 
 
 if __name__ == '__main__':
-    s0 = Song(0)
-    s0.load_song()
-    print(s0)
-    # s0.save_song("Bad Together .mp3", "2017", "", 238, 0)
-    # s1 = Song(1)
-    # s1.save_song("Be The One .mp3", "2017", "", 204, 0)
-    # s2 = Song(2)
-    # s2.save_song("Begging .mp3", "2017", "", 194, 0)
-    # s3 = Song(3)
-    # s3.save_song("Blow Your Mind .mp3", "2017", "", 173, 0)
+    s1 = Song(1)
+    s1.load()
+    print(s1)
+    #s0.save("Bad Together.mp3", "2017", "", 238)
+    #s1 = Song(1)
+    #s1.save("Be The One.mp3", "2017", "", 204)
+    #s2 = Song(2)
+    #s2.save("Begging.mp3", "2017", "", 194)
+    #s3 = Song(3)
+    #s3.save("Blow Your Mind.mp3", "2017", "", 173)
