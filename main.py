59f24f2f0e09c85cd8bd6af7e7bc62a7d9b6f7d0
Shehabeldeen <shehabeldeen555@gmail.com>
2018-12-06 18:21:39 +0200

    add songs names to list

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import inspect
 import re
 import sqlite3
 import urllib.request
 import tkinter as tk
+from tkinter import ttk
 from tkinter.filedialog import askdirectory
 
 import eyed3
@@ -11,34 +13,43 @@ qry = open("db/musicaly.sql").read()
 conn = sqlite3.connect('db/musicaly.db')
 conn.executescript(qry)
 
-#directory = askdirectory()
-#os.chdir(directory)
-def window():
-    root = tk.Tk()
-    root.title("Musicaly")
-    root.geometry('600x400')
-    root.configure(bg="black")
 
-    topFrame = tk.Frame(root, bg =  "")
-    topFrame.grid(row = 0)
-    leftFrame = tk.Frame(root, bg = "black")
-    leftFrame.grid(row = 3)
+# directory = askdirectory()
+directory = "/home/shehabeldeen/materials/concepts of programming/Musicaly/songs"
+os.chdir(directory)
 
-    appName = tk.Label(topFrame, text = "Musicaly", bg = "black", fg = "white")
-    appName.grid(column = 4)
+root = tk.Tk()
+root.title("Musicaly")
+root.geometry('800x600')
+root.configure(bg="black")
+
+leftFrame = tk.Frame(root, bg="black", height=600, width=25)
+leftFrame.grid(column=0, sticky="n")
+
+button1 = tk.Button(leftFrame, text="Songs", fg="white", bg="Black", width=20)
+button2 = tk.Button(leftFrame, text="Albums", fg="white", bg="Black", width=20)
+button3 = tk.Button(leftFrame, text="playlists", fg="white", bg="Black", width=20)
+button4 = tk.Button(leftFrame, text="Artists", fg="white", bg="Black", width=20)
+button5 = tk.Button(leftFrame, text="Bands", fg="white", bg="Black", width=20)
+button6 = tk.Button(leftFrame, text="genre", fg="white", bg="Black", width=20)
+
+button1.grid(row=0, padx=10, pady=5)
+button2.grid(row=1, padx=10, pady=5)
+button3.grid(row=2, padx=10, pady=5)
+button4.grid(row=3, padx=10, pady=5)
+button5.grid(row=4, padx=10, pady=5)
+button6.grid(row=5, padx=10, pady=5)
+
+separator = tk.Frame(root, bg="white", width=3, height=600)
+separator.grid(row=0, column=1)
+
+rightframe = tk.Frame(root, bg="black", width=400)
+rightframe.grid(row=0, column=2)
+
+list = tk.Listbox(rightframe, height=25, width=70)
+list.grid(row=0, column=0, padx=10, pady=5, sticky="n")
 
 
-    button1 = tk.Button(leftFrame, text = "Playlists", fg = "white", bg = "Black", width=20)
-    button2 = tk.Button(leftFrame, text = "Albums", fg = "white", bg = "Black", width=20)
-    button3 = tk.Button(leftFrame, text = "Artists", fg = "white", bg = "Black", width=20)
-
-    button1.grid(row = 1, ipadx = 10, pady= 5)
-    button2.grid(row = 2, ipadx = 10, pady= 5)
-    button3.grid(row = 3, ipadx = 10, pady= 5)
-
-    root.mainloop()
-
-window()
 for files in os.listdir(directory):
     if files.endswith(".mp3"):
         # get the real path of the song
@@ -48,16 +59,17 @@ for files in os.listdir(directory):
         audio = eyed3.load(path)
 
         # print the the name of the artist
-        print(audio.tag.artist)
+        # print(audio.tag.artist)
         artist = str(audio.tag.artist)
         artist = artist.lower()
         artist = artist.replace(" ", "")
 
         # print the the title of the song
-        print(audio.tag.title)
+        # print(audio.tag.title)
         song = str(audio.tag.title).replace(".mp3", "").lower()
-        song = tkinter.re.sub('[^0-9a-zA-Z]+', '', song)
-
+        song = tk.re.sub('[^0-9a-zA-Z]+', '', song)
+        list.insert("end", song)
+        # print(artist, song)
         # to get the lyrics of the song from azlyrics
         url = "http://www.azlyrics.com/lyrics/" + artist + "/" + song + ".html"
         text = urllib.request.urlopen(url)
@@ -71,15 +83,18 @@ for files in os.listdir(directory):
         lyrics = lyrics.replace("\\'", "\'")
         lyrics = lyrics.replace("<i>", "")
         lyrics = lyrics.replace("</i>", "")
-        print(lyrics)
+        # print(lyrics)
 
-        print(audio.tag.album)
-        print(audio.tag.track_num)
-        print(audio.tag.release_date)
+        # print(audio.tag.album)
+        # print(audio.tag.track_num)
+        # print(audio.tag.release_date)
         audio.tag.lyrics.set(lyrics)
-        print(audio.tag.lyrics[0].text)
-        print(audio.tag.genre.name)
-        print(audio.info.time_secs)
+        # print(audio.tag.lyrics[0].text)
+        # print(audio.tag.genre.name)
+        # print(audio.info.time_secs)
 
         # sng = Song(audio.tag.track_num)
         # sng.save_song(name=audio.tag.artist)
+
+
+root.mainloop()
